432a96458ad4570f5e5a04b25bea06d65ac72b87
Jotivirix <joignasa@gmail.com>
2015-11-06 17:11:29 +0100

    Inicio del Proyecto
    Solamente tiene una clase que pone un mensaje ("Hola GIT") en la
    pantalla

---
+++ src/Ejercicio1.java
@@ -0,0 +1,12 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class Ejercicio1 extends GraphicsProgram{
+	
+	public void run(){
+		GLabel etiqueta = new GLabel ("HOLA GIT");
+		etiqueta.setLocation(100,100);
+		add(etiqueta);
+	}
+
+}
