beede0eb63a2dcb045bd08683037be4aaa628fc3
Jotivirix <joignasa@gmail.com>
2015-11-06 19:23:19 +0100

    Finalizada la pirámide invertida
    Hemos creado una pirámide invertida. Ahora solo falta girarla.

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,12 +1,28 @@
 import acm.graphics.GLabel;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Ejercicio1 extends GraphicsProgram{
-	
+
 	public void run(){
-		GLabel etiqueta = new GLabel ("HOLA GIT");
-		etiqueta.setLocation(100,100);
-		add(etiqueta);
+		//Cambiamos el tamaño de la ventana
+		setSize (800,600);
+		//Llamo al método  que dibuja la pirámide
+		pintaPiramide();
 	}
 
+
+	public void pintaPiramide(){
+		int numLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
+
+
+		for(int j=0; j<numLadrillosBase; j++){
+			for(int i=0; i<numLadrillosBase-j; i++){
+				GRect ladrillo = new GRect (anchoLadrillo, altoLadrillo);
+				add(ladrillo,anchoLadrillo*i/2 + anchoLadrillo*j, altoLadrillo*i);
+			}
+		}
+	}
 }
