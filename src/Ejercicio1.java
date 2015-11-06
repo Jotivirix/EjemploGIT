eee48a59cd23efe6ff6199018c5746c09a0e2114
Jotivirix <joignasa@gmail.com>
2015-11-06 17:41:59 +0100

    Inicio del método pintaPiramide
    Hemos cambiado el código y puesto una pirámide

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -3,10 +3,14 @@ import acm.program.GraphicsProgram;
 
 public class Ejercicio1 extends GraphicsProgram{
 	
-	public void run(){
-		GLabel etiqueta = new GLabel ("HOLA GIT");
-		etiqueta.setLocation(100,100);
-		add(etiqueta);
+	public void run (){
+		//cambio el tamaño de la ventana
+		setSize(800,600);
+		//Llamo al método que dibuja la piramide
+		pintaPiramide();
 	}
-
+	
+	private void pintaPiramide(){
+		
+	}
 }
